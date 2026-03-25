d18858f977e1e4ab7888c37193261b9ec24f68b2
evan80751 <evanjelle31@gmail.com>
2026-03-25 03:29:42 +0000

    Nodemon and Dotenvx. -Part 1

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
+import '@dotenvx/dotenvx/config';
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import requestLogger from './middlewares/request-logger.js';
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
+import compression from 'compression';
+import helmet from 'helmet';
 
 var app = express();
+
+app.use(helmet());
+app.use(compression());
 
 app.use(requestLogger);
 app.use(express.json());
