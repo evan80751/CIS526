ac93834a3fb0dd848716ba452dae24e6118b963d
evan80751 <evanjelle31@gmail.com>
2026-04-16 16:27:35 -0500

    Completed Vue.js starter tutorial with authentication and profile page. -Tutorial5

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -3,65 +3,65 @@
  * @author Evan Jelle
  */
 
-// Import Libraies
-import axios from "axios";
+// Import Libraries
+import axios from 'axios'
 
 // Import Stores
-import { useTokenStore } from "@/stores/Token";
+import { useTokenStore } from '@/stores/Token'
 
-// Axios Instance Group
+// Axios Instance Setup
 const api = axios.create({
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-    },
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
 })
 
 // Add Interceptors
 const setupAxios = function () {
-    // Configure Request
-    api.interceptors.request.use(
-        (config) => {
-            if (config.url !== '/auth/token' && config.url !== '/api') {
-                const tokenStore = useTokenStore()
-                if (tokenStore.token.length > 0) {
-                    config.headers['Authorization'] = 'Bearer ' + tokenStore.token
-                }
-            }
-            return config 
-        },
-        (error) => {
-            return Promise.reject(error)
+  // Configure Requests
+  api.interceptors.request.use(
+    (config) => {
+      if (config.url !== '/auth/token' && config.url !== '/api') {
+        const tokenStore = useTokenStore()
+        if (tokenStore.token.length > 0) {
+          config.headers['Authorization'] = 'Bearer ' + tokenStore.token
         }
-    )
+      }
+      return config
+    },
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
 
-    // Configure Response
-    api.interceptors.response.use(
-        (res) => {
-            return res
-        },
-        async (err) => {
-            const config = err.config
-            if (config.url !== '/auth/token' && err.response) {
-                if (err.response.status === 401) {
-                    if (!config._retry) {
-                        config._retry = true
-                        try {
-                            const tokenStore = useTokenStore()
-                            await tokenStore.getToken()
-                            return api(config)
-                        } catch (error) {
-                            return Promise.reject(error)
-                        }
-                    } else {
-                        const tokenStore = useTokenStore()
-                        await tokenStore.getToken(true)
-                    }
-                }
+  // Configure Response
+  api.interceptors.response.use(
+    (res) => {
+      return res
+    },
+    async (err) => {
+      const config = err.config
+      if (config.url !== '/auth/token' && err.response) {
+        if (err.response.status === 401) {
+          if (!config._retry) {
+            config._retry = true
+            try {
+              const tokenStore = useTokenStore()
+              await tokenStore.getToken()
+              return api(config)
+            } catch (error) {
+              return Promise.reject(error)
             }
-            return Promise.reject(err)
+          } else {
+            const tokenStore = useTokenStore()
+            await tokenStore.getToken(true)
+          }
         }
-    )
+      }
+      return Promise.reject(err)
+    }
+  )
 }
 
 export { api, setupAxios }
